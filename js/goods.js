fc3132181063969f75bc916913c6e212eb30708a
leevscode <leevscode@gmail.com>
2023-06-05 18:10:36 +0900

    feat: company grid 작업 중

--- js/goods.js
+++ js/goods.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
   //   .catch((err) => console.log(err));
 
   let swGoods;
-  const SLIDECOUNT = 4;
+  // const SLIDECOUNT = 4;
 
   const getData = async function () {
     try {
@@ -23,9 +23,11 @@ window.addEventListener("load", function () {
     let html = ``;
     let copyArr = [..._data.goods];
 
-    if (copyArr.length <= SLIDECOUNT) {
-      copyArr = [..._data.goods, ..._data.goods];
-    }
+    // Swiper 버전에 따른 문제 발생
+    // 강제 목록 추가 제거
+    // if (copyArr.length <= SLIDECOUNT) {
+    //   copyArr = [..._data.goods, ..._data.goods];
+    // }
 
     copyArr.forEach((item, index, arr) => {
       let tag = `
@@ -51,8 +53,10 @@ window.addEventListener("load", function () {
 
   function makeSlideShow() {
     swGoods = new Swiper(".sw-goods", {
-      slidesPerView: 3,
       loop: true,
+      speed: 1000,
+      slidesPerView: 3,
+      spaceBetween: 20,
       navigation: {
         prevEl: ".sw-goods-prev",
         nextEl: ".sw-goods-next",
@@ -61,15 +65,30 @@ window.addEventListener("load", function () {
         delay: 1000,
         disableOnInteraction: false,
       },
+      breakpoints: {
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1400: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
     });
     swGoods.on("slideChange", function () {
-      let count = this.realIndex % SLIDECOUNT;
-      focusMenu(count);
+      // let count = this.realIndex % SLIDECOUNT;
+      // focusMenu(count);
+      focusMenu(this.realIndex);
     });
   }
 
   function focusMenu(_index) {
-    let lis = document.querySelectorAll(".goods-list li a");
+    let lis = document.querySelectorAll(".goods-list li");
     lis.forEach((item, index, arr) => {
       if (index === _index) {
         // 순서번호랑 슬라이드 번호가 같다면 add
@@ -99,7 +118,33 @@ window.addEventListener("load", function () {
         swGoods.slideToLoop(index);
       };
     });
+
+    focusMenu(0);
   }
 
   getData();
+
+  // 슬라이드 멈추기/재생하기
+  const bt = document.querySelector(".sw-goods-pause");
+  const icon = bt.querySelector(".fa-pause");
+
+  let swGoodsState = "play";
+  bt.onclick = (event) => {
+    const isPlaying = swGoodsState === "play";
+    swGoods.autoplay[isPlaying ? "stop" : "start"]();
+    swGoodsState = isPlaying ? "stop" : "play";
+    icon.classList.toggle("fa-play");
+    // if (swGoodsState === "play") {
+    //   // 슬라이드 멈춰라
+    //   swGoods.autoplay.stop();
+    //   swGoodsState = "stop";
+    //   icon.classList.add("fa-play");
+    // } else {
+    //   // 슬라이드 재실행
+    //   swGoods.autoplay.start();
+    //   swGoodsState = "play";
+    //   icon.classList.remove("fa-play");
+    // }
+  };
+  //------------- 재생 멈추기
 });
